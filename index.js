14fdbdedfcc9b6e4575671551c21ec18bdbdc68d
SethSchnrt <Seth.Schoenert@gmail.com>
2023-07-09 21:37:02 -0500

    added blurb link

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const blurbApp = document.getElementById('blurbApp');
+
+    blurbApp.addEventListener('click', (event) => {
+        window.location.href = 'https://blurb-44f4260f090e.herokuapp.com/';
+    });
+});
